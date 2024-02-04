6225220867bab4522ecf36bdb569722da484f5e0
Saurabh Vashistha <Saurabh Vashistha@DESKTOP-0H7DN9A>
2024-02-04 23:56:58 +0530

    String Commit

--- src/main/java/com/jenkins/order/OrderApplication.java
+++ src/main/java/com/jenkins/order/OrderApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderApplication {
 
 	public static void main(String[] args) {
+		System.out.println("STarting Application Order-Service");
 		SpringApplication.run(OrderApplication.class, args);
 	}
 
