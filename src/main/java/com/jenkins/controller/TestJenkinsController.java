5db593107612eb5eb92ac687a0dbdc32a03ca522
Saurabh Vashistha <Saurabh Vashistha@DESKTOP-0H7DN9A>
2024-02-04 19:24:18 +0530

    Chng Con

--- src/main/java/com/jenkins/controller/TestJenkinsController.java
+++ src/main/java/com/jenkins/controller/TestJenkinsController.java
@@ -1,6 +1,9 @@
 package com.jenkins.controller;
 
+import org.springframework.http.RequestEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +14,10 @@ public class TestJenkinsController {
 	@GetMapping("/testAPI")
 	public String getJenkinsTest() {
 		return "API Working";
+	}
+
+	@PostMapping
+	public String saveJenkinsObj(@RequestBody RequestEntity<String> sample) {
+		return "";
 	}
 }
